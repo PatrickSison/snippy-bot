de4ddaeadc637a17ec54e67f69f18f0cafaa2fcb
Patrick Sison <patrick.m.sison@gmail.com>
2020-03-07 14:21:03 -0500

    do the do

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config/default.json');
-const _ = require('lodash');
+
+console.log('Starting...')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+const { prefix, youtubeSecret, discordSecret } = config;
+
 client.on('message', msg => {
-  const message = msg.content.toLowerCase();
+	
+	if (msg.author.bot) return;
 
-  if (message === 're') {
-    msg.channel.send('REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
-  }
+	msg.content = msg.content.toLowerCase();
+	
+	if (false) {
 
-  if (message === 'betty dab') {
-    msg.channel.send('https://i.imgur.com/rwElyFd.gif')
-  }
+	} else {
+		switch (msg.content) {
+			case 're':
+				msg.channel.send('REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
+				break;
+			case 'betty dab':
+				msg.channel.send('https://i.imgur.com/rwElyFd.gif');
+				break;
+			case 'ligma':
+				msg.channel.send('https://media.giphy.com/media/wIyvbQa4g7CajwxkD5/giphy.gif');
+				msg.channel.send('LIGMA BALLS');
+			case 'sugma':
+				msg.channel.send('https://media.giphy.com/media/wIyvbQa4g7CajwxkD5/giphy.gif');
+				msg.channel.send('SUGMA DICK');
+			case 'gotta bounce':
+				msg.channel.send('GOTTA BOUNCE ON MAH BOYS DICK');	
+				msg.channel.send('https://media.giphy.com/media/wIyvbQa4g7CajwxkD5/giphy.gif');
+				msg.channel.send('https://media.giphy.com/media/wIyvbQa4g7CajwxkD5/giphy.gif');
+				msg.channel.send('https://media.giphy.com/media/wIyvbQa4g7CajwxkD5/giphy.gif');
+		}
+	}
 
-  if (message === 'ligma') {
     msg.channel.send('https://media.giphy.com/media/wIyvbQa4g7CajwxkD5/giphy.gif');
     msg.channel.send('LIGMA BALLS');
   }
 });
 
-client.login(_.get(config, 'discord.secret'));
+client.login(process.env.DISCORD_SECRET);
